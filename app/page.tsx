ec17217c79b944d72f246a2df570f07fab67c7b3
Travis <Travisang40@gmail.com>
2025-04-09 15:18:03 +0800

    remove video

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,7 @@ export default function Home() {
     <>
     <div className="flex flex-col justify-center items-center gap-9 px-3">
       <Header />
-      <iframe src="https://www.youtube.com/embed/g9G218IncLw?si=D0BujSPHJCgu9cuK"
-      className="max-w-[1150px] bg-red-400 w-full h-[700px]" 
-      title="YouTube video player" 
-      id="video"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
-      </iframe>
+      
       <Features />
       <Reviews />
       <Pricing />
